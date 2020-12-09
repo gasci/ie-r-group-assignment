17171a45064c5969a6840c9b2bc1e1e67ec1af65
Iñigo Hidalgo Rey <inigo@Inigos-MacBook-Pro.local>
2020-12-09 11:05:48 +0100

    simplified na counting and distinct values functions

--- wrangling.r
+++ wrangling.r
@@ -1,25 +1,33 @@
 #TODO: intro
-
+load("data-wrangling.RData")
 library(readxl)
 library(tidyverse)
+library(modelr)
 library(reshape2)
-# import excel dataset and adjust column types
-# TODO: change to csv
-accounts <- read_excel("data.xlsx",
-                       col_types = c("text", "text", "text", 
-                                     "text", "numeric", "text", "text", 
-                                     "numeric", "text", "text", "text", 
-                                     "text", "numeric", "numeric", "text", 
-                                     "text", "text", "text", "text", "text", 
-                                     "numeric", "text", "numeric", "numeric", 
-                                     "numeric", "numeric", "numeric", 
-                                     "numeric", "numeric", "numeric", 
-                                     "numeric", "numeric", "numeric", 
-                                     "numeric", "numeric", "numeric", 
-                                     "numeric", "numeric", "numeric", 
-                                     "numeric", "numeric", "text", "text", 
-                                     "text", "text", "numeric", "numeric"))
+# TODO: research rocr
+library(ROCR)
+# Alt + - for <- 
+# Ctrl + Shift + M for %>% 
 
+# import excel dataset and adjust column types
+# TODO: change to csv (maybe not)
+# accounts <- read_excel("data.xlsx",
+#                        col_types = c("text", "text", "text", 
+#                                      "text", "numeric", "text", "text", 
+#                                      "numeric", "text", "text", "text", 
+#                                      "text", "numeric", "numeric", "text", 
+#                                      "text", "text", "text", "text", "text", 
+#                                      "numeric", "text", "numeric", "numeric", 
+#                                      "numeric", "numeric", "numeric", 
+#                                      "numeric", "numeric", "numeric", 
+#                                      "numeric", "numeric", "numeric", 
+#                                      "numeric", "numeric", "numeric", 
+#                                      "numeric", "numeric", "numeric", 
+#                                      "numeric", "numeric", "text", "text", 
+#                                      "text", "text", "numeric", "numeric"))
+# "takes a while, we won't bother with making you wait"
+
+glimpse(accounts)
 # column type conversion
 accounts.recast <- accounts %>%
   mutate(customerType = as.numeric(customerType), # convert customerType to numeric
@@ -33,7 +41,8 @@ accounts.cleaned <- accounts.recast %>%
   mutate(birthPlace = str_replace(birthPlace, "[?]", replacement=NA_character_)) 
 
 # calculate percentage of missing NAs
-percentage.na <- accounts.cleaned %>% summarise(across(everything(), ~ sum(is.na(.x))/length(.x)))
+(percentage.na <- accounts.cleaned %>% summarise(across(everything(), ~ sum(is.na(.x))/length(.x))))
+
 
 # discarding columns with more than 50% of missing values
 accounts.tidy <- select(accounts.cleaned,
@@ -50,21 +59,22 @@ accounts.tidy <- select(accounts.cleaned,
                         ))
 
 
-# discarding dateOfBirth because it gives no extra information
+# discarding dateOfBirth because it gives no extra information over Age
 accounts.tidy <- select(accounts.tidy,
                         -c(dateOfBirth))
 
 # discarding ledgerCode because it is just a random number
 accounts.tidy <- select(accounts.tidy,
                         -c(ledgerCode))
+# TODO: move unique_counter and na_counter to beginning and clarify
+map_dbl(accounts.tidy, unique_counter)
 
 # discarded because the columns are the same for everybody
 accounts.tidy <- select(accounts.tidy,
                         -c(IsBlackListed,
                            org_code,
                            status,
-                           date_of_assessment
-                        ))
+                           date_of_assessment))
 
 # encoding rbagradedesc to 0 if low or 1 if medium or high
 accounts.tidy$rba_grade_desc[accounts.tidy$rba_grade_desc != "Low"] <- 1
@@ -88,12 +98,26 @@ accounts.tidy.factored <- accounts.tidy %>%  mutate_at(c('customerType',
 accounts.tidy.factored.replaced_nas <- accounts.tidy.factored %>%  
     replace_na(list(age_in_year=median(accounts.tidy.factored$age_in_year, na.rm = TRUE)))
 
-# dropping rows for other columns that have NA values since the NA ratio is low
-accounts.tidy.factored.replaced_nas.dropped <- accounts.tidy.factored.replaced_nas %>% filter(!is.na(nationalityOriginal)) %>% 
-  filter(!is.na(residentCountry)) %>% filter(!is.na(LEGAL_STA_CODE)) 
+na_counter <- function(x) sum(is.na(x))
 
-sapply(accounts.tidy.factored.replaced_nas.dropped, function(x) sum(is.na(x)))
+map_dbl(accounts.tidy.factored.replaced_nas, na_counter)
 
+# dropping observations for other columns that have NA values since the NA ratio is low
+accounts.tidy.factored.replaced_nas.dropped <- accounts.tidy.factored.replaced_nas %>% 
+  filter(!is.na(nationalityOriginal)) %>% 
+  filter(!is.na(residentCountry)) %>% 
+  filter(!is.na(LEGAL_STA_CODE)) 
+map_dbl(accounts.tidy.factored.replaced_nas.dropped, na_counter)
+rows_dropped <- nrow(accounts.tidy.factored.replaced_nas) - nrow(accounts.tidy.factored.replaced_nas.dropped)
+
+print(paste("Dropped", rows_dropped, "rows out of", nrow(accounts.tidy.factored.replaced_nas)))
+
+# Number of NAs per column
+# sapply(accounts.tidy.factored.replaced_nas.dropped, function(x) sum(is.na(x)))
+map_dbl(accounts.tidy.factored.replaced_nas.dropped, na_counter)
+
+unique_counter <- function(x) length(unique(x))
+map_dbl(accounts.tidy.factored.replaced_nas.dropped, unique_counter)
 
 # showing unique value for each column
 unique.values <- accounts.tidy.factored.replaced_nas %>% summarise(across(everything(), ~ length(unique(.x))))
@@ -103,9 +127,16 @@ percentage.na <- accounts.tidy.factored.replaced_nas.dropped %>% summarise(acros
 
 # view of our final data set to use in the logit regression model
 View(accounts.tidy.factored.replaced_nas.dropped)
+# TODO: encode categorical variable as flag
 
 
-numeric.accounts.tidy <-accounts.tidy.factored.replaced_nas.dropped %>% select_if(is.numeric)
+is.numeric(c(1,2))
+# select numeric only since it is the only valid way to do a model
+numeric.accounts.tidy <- accounts.tidy.factored.replaced_nas.dropped %>% 
+  select_if(is.numeric)
+
+map_lgl(accounts.tidy.factored.replaced_nas.dropped, is.numeric)
+
 result <- round(cor(numeric.accounts.tidy), 2)
 melted_cormat <- melt(result)
 head(melted_cormat)
@@ -114,13 +145,18 @@ head(melted_cormat)
 ggplot(data = melted_cormat, aes(x=Var1, y=Var2, fill=value)) + 
   geom_tile()
 
+# TODO: print labels diagonally
 
 # we find that avg_last_90, 30 and 10 are highly correlated
 # we could do PCA but we will discard 2 of them
 
-numeric.accounts.tidy.removed.cor <- numeric.accounts.tidy %>% select(-c(avg_last_30_days, avg_last_10_days))
+numeric.accounts.tidy.removed.cor <- numeric.accounts.tidy %>% 
+  select(-c(avg_last_30_days, avg_last_10_days))
 
 View(accounts.tidy.factored.replaced_nas.dropped)
+
+
+# modeling
 
 # create another data set name accounts.logit
 # remove rbaValue from the dataset
@@ -142,13 +178,13 @@ test <- accounts.logit[-train_ind, ]
 sapply(train, function(x) sum(is.na(x)))
 
 # build the logit regression model
-logit.model <- glm(rba_grade_desc ~ ., family=binomial(link='logit'), maxit = 100, data=test)
-
+logit.model <- glm(rba_grade_desc ~ ., family=binomial(link='logit'), maxit = 100, data=train)
+# TODO: give more info on glm function
 summary(logit.model)
 # could remove insignificant variables
 
 #calculate AUC 
-library(ROCR)
+
 p <- predict(logit.model,newdata=train,type='response')
 pr <- prediction(p, train$rba_grade_desc)
 auc <- performance(pr, measure = "auc")
@@ -167,7 +203,7 @@ auc
 # similar
 
 
-library(modelr)
+
 
 ##liner model
 
